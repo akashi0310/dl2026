47a97984606ccacac40cf5de96dbdf4dcab5c69a
karma-san <daohoangdung2k4@gmail.com>
2026-05-06 20:17:18 +0700

    grad descent

---
+++ labwork1.py
@@ -0,0 +1,20 @@
+def f(x):
+    return x**2
+
+def df(x):
+    return 2 * x
+
+L = 0.1 # learning rate
+
+def gradient_descent(x):
+    x = x - L * df(x)
+    return x
+
+x = 10
+print("Time   x      f(x)")
+for i in range(1,11): 
+    x = gradient_descent(x)
+    if x == 1:
+        break
+    print(f"{i}      {x:.2f}   {f(x):.2f}")
+    
